feec22b3e899096418fbf9f2be97b96c42e70171
SarojKumarRanjan <sarojranjan555@gmail.com>
2023-09-16 17:37:17 +0530

    salting and hashing

--- app.js
+++ app.js
@@ -1,80 +1,91 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
-
+const bcrypt = require("bcrypt");
+const saltRound = 10;
 
 const app = express();
 app.use(express.static("public"));
-app.set('view engine','ejs');
-app.use(bodyparser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://127.0.0.1:27017/userDB', {useNewUrlParser: true});
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String
+  email: String,
+  password: String,
 });
 
 // secret key
 
-
-
 const User = new mongoose.model("User", userSchema);
 
-app.get("/",function(req,res){
-    res.render("home");
-})
-
-app.get("/login",function(req,res){
-    res.render("login");
-})
-
-
-app.get("/register",function(req,res){
-    res.render("register");
-})
-
-
-app.post("/register", function(req,res){
-    const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-
-    newUser.save();
-    res.render("secrets");
-})
-
-
-app.post("/login", function(req, res) {
-    const username = req.body.username;
-    const password = req.body.password;
-
-    User.findOne({ email: username })
-        .then((foundUser) => {
-            if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get("/", function (req, res) {
+  res.render("home");
 });
 
+app.get("/login", function (req, res) {
+  res.render("login");
+});
 
+app.get("/register", function (req, res) {
+  res.render("register");
+});
 
+app.post("/register", function (req, res) {
+  bcrypt
+    .hash(req.body.password, saltRound)
 
+    .then((hash) => {
+      const newUser = new User({
+        email: req.body.username,
+        password: hash,
+      });
 
+      newUser.save();
+      res.render("secrets");
+    })
 
+    .catch((err) => {
+      console.error("Error:", err);
+    });
+});
 
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  User.findOne({ email: username })
+    .then((foundUser) => {
+      if (foundUser) {
+        bcrypt
+          .compare(password, foundUser.password)
+          .then((isMatch) => {
+            if (isMatch) {
+              res.render("secrets");
+            } else {
+              // Password doesn't match
+              res.render("login"); // You can render the login page again or redirect as needed
+            }
+          })
+          .catch((err) => {
+            console.error("Error comparing passwords:", err);
+            res.render("login"); // Handle the error by rendering the login page again
+          });
+      } else {
+        // User not found
+        res.render("login"); // You can render the login page again or redirect as needed
+      }
+    })
+    .catch((err) => {
+      console.error("Error finding user:", err);
+      res.render("login"); // Handle the error by rendering the login page again
+    });
+});
 
-app.listen(3000,function(){
-    console.log("server started");
+app.listen(3000, function () {
+  console.log("server started");
 });
